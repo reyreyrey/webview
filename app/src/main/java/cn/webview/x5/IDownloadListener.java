4b6ad33ca742a5529e1db20b0e790a2b83bd9ead
wiki <wiki@damons-iMac.local>
2018-03-08 16:36:22 +0800

    1

---
+++ app/src/main/java/cn/webview/x5/IDownloadListener.java
@@ -0,0 +1,27 @@
+package cn.webview.x5;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tencent.smtt.sdk.DownloadListener;
+
+import cn.webview.x5.download.download.DownloadAppService;
+import cn.webview.x5.download.download.UpdateProgressDialog;
+
+
+public class IDownloadListener implements DownloadListener {
+    private AppCompatActivity context;
+
+    public IDownloadListener(Context context) {
+        this.context = (AppCompatActivity) context;
+    }
+
+    @Override
+    public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long length) {
+        DownloadAppService.downloadAppAndInstall(context, url, true);
+        UpdateProgressDialog dialog = new UpdateProgressDialog();
+        dialog.show(context.getSupportFragmentManager(), "update");
+    }
+
+
+}
